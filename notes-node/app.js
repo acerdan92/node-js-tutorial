bba01e7e8aa27a5df3b387f6f6df6af4a0a7059e
Antonio Yerena <acerdanyerena92@hotmail.com>
2018-05-19 13:18:29 -0500

    Add node tutorial app section 3, exercise 3

---
+++ notes-node/app.js
@@ -0,0 +1,15 @@
+console.log('Starting app.js');
+
+const fs = require('fs');
+const os = require('os');
+const notes = require('./notes.js');
+
+console.log('Result: ', notes.add(9, 5));
+
+var user = os.userInfo();
+
+fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age}.`, function (err) {
+  if (err) {
+    console.log('Unable to write to file');
+  }
+});
